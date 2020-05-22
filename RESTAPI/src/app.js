5441c5a1c8c74d12d6b8eeae8320c541e3ffbde3
pedro <pedro-manuel16@hotmail.com>
2020-05-22 19:46:17 +0100

    Adicionada Base de dados de Teste

---
+++ RESTAPI/src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+const app = express();
+const routes = require('./routes/general_routes');
+//Middlewares
+
+app.use(cors());
+app.use(bodyparser.json());
+app.use(require('./database/database'));
+
+//Routes
+app.use('/', routes);
+
+//Server listen 3000
+app.listen(3000,()=>{
+    console.log('Listening on :3000');
+});
---
+++ RESTAPI/src/database/database.js
@@ -0,0 +1,16 @@
+// Require Neo4j
+const Neode = require('neode');
+
+//Test BD
+const password = 'teste';
+const user = 'pedro';
+
+const instance = new Neode('bolt://localhost:7687', 
+user, password);
+
+// Express middleware
+module.exports = function(req, res, next) {
+    req.instance = instance;
+    
+    next();
+};
