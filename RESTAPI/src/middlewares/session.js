6f46650c46ef5587188076ce7db5b503ecafb0b7
pedro <pedro-manuel16@hotmail.com>
2020-06-01 22:12:13 +0100

    Adicionado 2 middlewares para controlo de sessão

---
+++ RESTAPI/src/middlewares/session.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const jwtConfig  =require('../config/auth.json');
+
+const sessionMiddleware = (req, res, next) => {
+
+var session = req.headers['x-access-token'];
+
+	try {
+		if (session) {
+			const user = jwt.verify(session, jwtConfig.secret);
+			req.auth = user;
+		} else {
+			req.auth = null;
+		}
+
+	} catch (e) {
+		req.auth = null;
+	}
+
+	next();
+} 
+
+module.exports = sessionMiddleware
