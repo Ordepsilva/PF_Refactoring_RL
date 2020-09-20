c4baa7d9e3a312c6039d8ff652e1e7baf2bb621c
pedro <pedro-manuel16@hotmail.com>
2020-09-20 18:22:04 +0100

    Actualização no serviço de validação

--- Front-End/FrontEnd/src/app/services/validation.service.ts
+++ Front-End/FrontEnd/src/app/services/validation.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from "@angular/core";
+import { MatDialog } from '@angular/material/dialog';
+import { InfoDialogComponent } from '../components/MainComponent/info-dialog/info-dialog.component';
 
 @Injectable({
   providedIn: "root"
 })
 export class ValidationService {
-  constructor() {}
+  constructor( public dialog: MatDialog) {}
 
   /**
    * This function validate a username.
    */
   validateUsername(username: string): boolean {
     if (username === undefined) {
-      alert("Username must be filled.");
+      const dialogRef = this.dialog.open(InfoDialogComponent, {
+        width: "400px", data: {
+          message: "Username must be filled",
+          type: "failed"
+        }
+      });
       return false;
     } else {
       return true;
@@ -23,10 +30,20 @@ export class ValidationService {
    */
   validatePassword(password: string, cpassword: string): boolean {
     if (password === undefined) {
-      alert("Password must be filled.");
+      const dialogRef = this.dialog.open(InfoDialogComponent, {
+        width: "400px", data: {
+          message: "Password must be filled",
+          type: "failed"
+        }
+      });
       return false;
     } else if (password != cpassword) {
-      alert("Passwords don´t match.");
+      const dialogRef = this.dialog.open(InfoDialogComponent, {
+        width: "400px", data: {
+          message: "Passwords don't match",
+          type: "failed"
+        }
+      });
       return false;
     } else {
       return true;
