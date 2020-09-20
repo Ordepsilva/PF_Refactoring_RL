76995ef30784320166aabc49a34c83cc02d7c1d3
pedro <pedro-manuel16@hotmail.com>
2020-09-20 18:22:27 +0100

    Atualização e correção de erros no swagger

--- RESTAPI/app.js
+++ RESTAPI/app.js
@@ -2,8 +2,35 @@ const express = require('express');
 const cors = require('cors');
 const bodyparser = require('body-parser');
 const app = express();
-var session = require('./src/middlewares/session');
+const session = require('./src/middlewares/session');
 const routes = require('./src/routes/generalRoutes');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Rest API',
+            description: "REST API Information",
+            contact: {
+                name: "Pedro Silva"
+            },
+            servers: ["http://localhost:3000"]
+        },
+        securityDefinitions:{
+            bearerAuth: {
+                type: 'apiKey',
+                name: 'x-access-token',
+                scheme: 'bearer',
+                in: 'header'
+            },
+        },
+    },
+    apis: ['./src/routes/*.js', './src/models/*js']
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 //Middlewares
 app.use(cors());
@@ -14,6 +41,6 @@ app.use(session);
 app.use('/', routes);
 
 //Server listen 3000
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('Listening on :3000');
 });
